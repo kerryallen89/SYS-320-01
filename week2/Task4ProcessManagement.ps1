f189fe48513d749a6b2c3f953391dd5a4d95045f
Kerry <kerry.allen@mymail.champlain.edu>
2025-01-29 12:32:51 -0500

    Task 4 for Week2

---
+++ week2/Task4ProcessManagement.ps1
@@ -0,0 +1,7 @@
+﻿#Task 4:
+$chrome = Get-Process | Where-Object {$_.ProcessName -eq "chrome" }
+if ($chrome) {
+    Stop-Process -Name "chrome" -Force
+} else {
+Start-Process "chrome.exe" -ArgumentList "https://www.champlain.edu"
+}
