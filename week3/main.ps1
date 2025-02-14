dc068686d83f453ce780090b86fdaf17fba5f8fb
Kerry <kerry.allen@mymail.champlain.edu>
2025-02-13 22:33:33 -0500

    Used to call functions from first script file

---
+++ week3/main.ps1
@@ -0,0 +1,15 @@
+﻿.(Join-Path $PSScriptRoot FunctionEventLogs.ps1)
+
+clear
+
+# Get Login and Logoffs from the last 15 days
+$loginoutsTable = Get-LoginEvents -DaysAgo 15
+$loginoutsTable
+
+# Get shutdown from the last 25 days
+$shutdownsTable = Get-SystemEvents -DaysAgo 25 Where-Object {$_.Event -eq "Shutdown"}
+$shutdownsTable
+
+# Get Startup from the last 25 days
+$startupsTable = Get-SystemEvents -DaysAgo 25 Where-Object {$_.Event -eq "Startup"}
+$startupsTable
