bb8ff5d0ac65b82007233f6d5bce09e9f8aa573c
Kerry <kerry.allen@mymail.champlain.edu>
2025-02-13 22:31:28 -0500

    Finished Code w/ working Functions

--- week3/FunctionEventLogs.ps1
+++ week3/FunctionEventLogs.ps1
@@ -1,6 +1,12 @@
-﻿$loginouts = Get-EventLog system -source Microsoft-Windows-WinLogon -After (Get-Date).AddDays(-14)
+﻿clear 
+function Get-LoginEvents {
+    param(
+        [int]$DaysAgo
+    )
 
+$loginouts = Get-EventLog system -source Microsoft-Windows-WinLogon -After (Get-Date).AddDays(-$DaysAgo)
 $loginoutsTable = @()
+
 for($i=0; $i -lt $loginouts.Count; $i++){
 
 $event = ""
@@ -16,6 +22,46 @@ $loginoutsTable += [PSCustomObject]@{"Time" = $loginouts[$i].TimeGenerated;
                                     "Event" = $event;
                                      "User" = $user;
                                     }
+    }
+
+    return $loginoutsTable
 }
 
-$loginoutsTable
+function Get-SystemEvents {
+    param(
+        [int]$DaysAgo
+    )
+    
+    $systemEvents = Get-EventLog system -After (Get-Date).AddDays(-$DaysAgo) |
+                    Where-Object {$_.EventId -eq 6005 -or $_.EventId -eq 6006}
+    
+    $systemEventsTable = @()
+    
+    for ($i = 0; $i -lt $systemEvents.Count; $i++) {
+
+    $event = ""
+    if ($systemEvents[$i].EventId -eq 6005) {$event = "Startup"}
+    if ($systemEvents[$i].EventId -eq 6006) {$event = "Shutdown"}
+    
+    $systemEventsTable += [PSCustomObject]@{"Time" = $systemEvents[$i].TimeGenerated;
+                                              "Id" = $systemEvents[$i].EventID;
+                                           "Event" = $event
+                                            "User" = "System"
+                                           }
+    }
+
+    return $systemEventsTable
+}
+
+
+# Calls both funtions and prints the results
+$DaysToRetrieve = 14
+
+
+$loginResults = Get-LoginEvents -DaysAgo $DaysToRetrieve
+Write-Output "Logon/Logoff Events:"
+$loginResults
+
+$systemResults = Get-SystemEvents -DaysAgo $DaysToRetrieve
+Write-Output "`nSystem Startup/Shutdown Events:"
+$systemResults
