dba32bcb05f30243ac20b78c36371be4eea03aff
Kerry <kerry.allen@mymail.champlain.edu>
2025-02-13 19:56:32 -0500

    Deliverable 3 WORKING

---
+++ week3/FunctionEventLogs.ps1
@@ -0,0 +1,21 @@
+﻿$loginouts = Get-EventLog system -source Microsoft-Windows-WinLogon -After (Get-Date).AddDays(-14)
+
+$loginoutsTable = @()
+for($i=0; $i -lt $loginouts.Count; $i++){
+
+$event = ""
+if($loginouts[$i].InstanceId -eq 7001) {$event = "Logon"}
+if($loginouts[$i].InstanceId -eq 7002) {$event = "Logoff"}
+
+#Translate the SID to a username
+$sid = New-Object System.Security.Principal.SecurityIdentifier($loginouts[$i].ReplacementStrings[1]) 
+$user = $sid.Translate([System.Security.Principal.NTAccount]).Value
+    
+$loginoutsTable += [PSCustomObject]@{"Time" = $loginouts[$i].TimeGenerated;
+                                       "Id" = $loginouts[$i].InstanceId;
+                                    "Event" = $event;
+                                     "User" = $user;
+                                    }
+}
+
+$loginoutsTable
