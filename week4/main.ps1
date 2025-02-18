a6c5b33faf1c2d1d5beb95de1cc82f01386e5e2d
Kerry <kerry.allen@mymail.champlain.edu>
2025-02-18 00:24:22 -0500

    There is something wrong with this, not sure what tho

---
+++ week4/main.ps1
@@ -0,0 +1,11 @@
+﻿clear
+
+.(Join-Path $PSScriptRoot Apache-Logs.ps1)
+.(Join-Path $PSScriptRoot ParsingApacheLogs.ps1)
+
+#Used to test WindowsApacheLogs
+apache-logs SummerCivilian 404 chrome
+
+#Used to test ParsingApacheLogs
+$test = apacheLogFilter
+$test | Format-Table -AutoSize -Wrap
