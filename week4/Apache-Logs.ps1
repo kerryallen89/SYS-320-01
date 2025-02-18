7d79a16d838788f27ea86f4d307d76d5865a3b53
Kerry <kerry.allen@mymail.champlain.edu>
2025-02-17 23:40:20 -0500

    Not sure if its there supposed to be a return or not

---
+++ week4/Apache-Logs.ps1
@@ -0,0 +1,22 @@
+﻿clear
+
+function apache-logs ($page, $httpCode, $webBrowser){
+    cd C:\xampp\apache\logs
+    $logSearch = Get-Content C:\xampp\apache\logs\access.log | Select-String $page | Select-String $httpCode
+    $notfound = $logSearch | Select-String $webBrowser
+
+    #Define a regex for IP addresses
+    $regex = [regex] "\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+
+    #Get $notfounds records that match to the regex
+    $ipsUnorganized = $regex.Match($notfound)
+
+    #Get ips as pscustomobject
+    $ips = @()
+    for($i=0; $i -lt $ipsUnorganized.Count; $i++){
+        $ips += [PSCustomObject]@{ "IP" = $ipsUnorganized[$i].value;}
+    }
+    $ipsoftens = $ips | Where-Object { $_.IP -ilike "10.*" }
+    $count = $ipsoftens | Group
+    $count | Select-Object Count, Name
+}
